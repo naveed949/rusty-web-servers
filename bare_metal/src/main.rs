0674ebb7fa45cc71314c29e8fc450a7207903a4a
naveed949 <naveediqbal949@gmail.com>
2024-09-30 15:41:40 +0500

    feat: add json response handler

--- bare_metal/src/main.rs
+++ bare_metal/src/main.rs
@@ -1,7 +1,14 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use threadpool::ThreadPool;
+use serde::Serialize;
+use serde_json::json;
 
+#[derive(Serialize)]
+struct JsonResponse {
+    message: String,
+    data: Option<String>,
+}
 const OK_STATUS: &str = "HTTP/1.1 200 OK";
 const NOT_FOUND_STATUS: &str = "HTTP/1.1 404 NOT FOUND";
 
@@ -36,21 +43,30 @@ fn handle_connection(mut stream: TcpStream) {
         }
     };
 
-    let (status_line, contents) = match request_line.as_str() {
-        "GET / HTTP/1.1" => (OK_STATUS, "Hello, GET!".to_string()),
-        "POST / HTTP/1.1" => (OK_STATUS, "Hello, POST!".to_string()),
+    let (status_line, contents, contentType) = match request_line.as_str() {
+        "GET / HTTP/1.1" => (OK_STATUS, "Hello, GET!".to_string(), "text/plain"),
+        "POST / HTTP/1.1" => (OK_STATUS, "Hello, POST!".to_string(), "text/plain"),
         req if req.starts_with("GET /?param=") => {
             println!("Request: {}", req);
             let param_value = &req[12..req.find(" HTTP/1.1").unwrap_or(req.len())];
             let content = format!("Hello, GET with param: {}", param_value);
-            (OK_STATUS, content)
+            (OK_STATUS, content, "text/plain")
         },
-        _ => (NOT_FOUND_STATUS, "Not Found".to_string()),
+        "GET /json HTTP/1.1" => {
+            let response = JsonResponse {
+                message: "Hello, JSON!".to_string(),
+                data: None,
+            };
+            let content = json!(response).to_string();
+            (OK_STATUS, content, "application/json")
+        },
+        _ => (NOT_FOUND_STATUS, "Not Found".to_string(), "text/plain"),
     };
 
     let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
         status_line,
+        contentType,
         contents.len(),
         contents
     );
