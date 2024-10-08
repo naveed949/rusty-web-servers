71531db2eb5e5c501fbe14b22b1a8d9425cc04de
naveed949 <naveediqbal949@gmail.com>
2024-10-08 14:17:26 +0500

    feat: add 404 fallback route to warp server

--- warp_server/src/main.rs
+++ warp_server/src/main.rs
@@ -6,8 +6,15 @@ async fn main() {
     let hello = warp::path!("hello")
         .map(|| warp::reply::html("Hello, Warp!"));
 
+    // fallback to 404
+    let fallback = warp::any()
+        .map(|| warp::reply::with_status("Invalid Route!", warp::http::StatusCode::NOT_FOUND));
+
+    // Combine the two filters with `or`
+    let routes = hello.or(fallback);
+
     // Start the server
-    warp::serve(hello)
+    warp::serve(routes)
         .run(([127, 0, 0, 1], 8080))
         .await;
 }
