07f84b61adeab940e7d6d953a51d4814df61bc4d
naveed949 <naveediqbal949@gmail.com>
2024-10-14 13:34:34 +0500

    feat: add logging to request handlers and enhance error reporting

--- warp_server/src/handler.rs
+++ warp_server/src/handler.rs
@@ -1,3 +1,4 @@
+use log::{error, info};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use warp::Filter;
@@ -10,15 +11,21 @@ use crate::{
 // Define the handler functions (filters in warp)
 
 pub fn hello() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("hello").map(|| warp::reply::html("Hello, Warp!"))
+    warp::path!("hello").map(|| {
+        info!("Received request for /hello");
+        warp::reply::html("Hello, Warp!")
+    })
 }
 
 pub fn greet_with_path_variable(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path!("greet" / String).map(|name| warp::reply::html(format!("Hello, {}!", name)))
+    warp::path!("greet" / String).map(|name| {
+        info!("Received request for /greet/{}", name);
+        warp::reply::html(format!("Hello, {}!", name))
+    })
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 struct GreetRequest {
     name: String,
     age: u8,
@@ -30,6 +37,10 @@ pub fn greet_with_query_parameters(
         .and(warp::get())
         .and(warp::query::<GreetRequest>())
         .map(|query: GreetRequest| {
+            info!(
+                "Received request for /greet with query parameters: {:?}",
+                query
+            );
             warp::reply::html(format!(
                 "Hello, {}! You are {} years old.",
                 query.name, query.age
@@ -39,6 +50,7 @@ pub fn greet_with_query_parameters(
 
 pub fn serve_static_files(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    info!("Serving static files from /static");
     warp::path("static").and(warp::fs::dir("static"))
 }
 
@@ -49,9 +61,10 @@ pub fn todo_list_json(
         .and(warp::get())
         .and(warp::path::end())
         .and(with_db(db.clone()))
-        .map(|dbPool: InMemoryDB| {
-            let todos = dbPool.read_all_items();
-
+        .map(|db_pool: InMemoryDB| {
+            info!("Received request for /todo");
+            let todos = db_pool.read_all_items();
+            info!("Fetched todos: {:?}", todos);
             warp::reply::json(&todos)
         })
 }
@@ -63,11 +76,18 @@ pub fn todo_list_json_with_id(
         .and(warp::get())
         .and(with_db(db.clone()))
         .map(|id: u64, db_pool: InMemoryDB| {
+            info!("Received request for /todo/{}", id);
             let todo = db_pool.read_item(id);
 
             match todo {
-                Some(todo) => warp::reply::json(&todo),
-                None => warp::reply::json(&"Todo not found"),
+                Some(todo) => {
+                    info!("Found todo: {:?}", todo);
+                    warp::reply::json(&todo)
+                }
+                None => {
+                    error!("Todo with id {} not found", id);
+                    warp::reply::json(&"Todo not found")
+                }
             }
         })
 }
@@ -87,12 +107,16 @@ pub fn todo_list_set_json(
         .and(with_auth())
         .and(middleware::with_db(db.clone()))
         .map(|todo: TodoRequest, user: User, db_pool: InMemoryDB| {
-            log::info!("Received todo: {:?} from user: {}", todo, user.name);
+            info!("Received todo: {:?} from user: {}", todo, user.name);
             let item = db_pool.create_item(todo.title, todo.completed);
+            info!("Created todo with id: {}", item.id);
             warp::reply::json(&format!("Created todo with id: {}", item.id))
         })
 }
 // Fallback route
 pub fn fallback() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path::end().map(|| warp::reply::html("No Route Found"))
+    warp::path::end().map(|| {
+        error!("No route found for the request");
+        warp::reply::html("No Route Found")
+    })
 }
